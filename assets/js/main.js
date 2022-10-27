4aa42e34ea2b331baf5075ac41fc46f639b775ca
Yuri-Punzo <yuripunzo@hotmail.it>
2022-10-27 18:13:14 +0200

    esercizio "base" finito

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,127 +47,17 @@ createApp({
         }
     },
     methods: {
-
+        nextFunction() {
+            this.visibleElement++;
+            if (this.visibleElement === this.games.length) {
+                this.visibleElement = 0;
+            }
+        },
+        prevFunction() {            
+            this.visibleElement--;
+            if (this.visibleElement === -1) {
+                this.visibleElement = 4;
+            }
+        }
     }
 }).mount("#app")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// seleziono dove mettere le immagini
-const slidesElement = document.querySelector(".carousel");
-
-// getto la base per il mio counter e per poi passare alle imagini successive con ++ 
-   //(inoltre se vado a cambiare "= 0" in "= X" posso far partire le slide da un img diversa)
-let visibleImg = 0;
-
-for (let i = 0; i < slidesList.length; i++) {
-    const slideListObjects = slidesList[i];
-    //console.log(slideListObjects);
-    //console.log(slideListObjects.image);
-    const slidesHtml = `<h4 class="${i === visibleImg ? 'visible' : ''}">${slideListObjects.title}</h4><p class="${i === visibleImg ? 'visible' : ''}">${slideListObjects.text}</p><img class="${i === visibleImg ? 'visible' : ''}" src="./assets/${slideListObjects.image}" alt="">`
-    slidesElement.insertAdjacentHTML("beforeend", slidesHtml)
-    //console.log(slidesHtml);
-}
-
-// creo due variabile che selezionino i pulsanti per farli poi funzionare
-const prevButton = document.getElementById("previous");
-const nextButton = document.getElementById("next");
-
-// adesso voglio cambiare immagine con il click di next:
-// vado a selezionare tutte le immagini con una var allImg
-// creo una variabile (currentSlide) per poter selezionare l'img visibile al momento
-// (dato che visibleImg ha valore zero pescherà la prima immagine)
-// poi gli tolgo la classe visible con classlist.remove facendola sparire
-// devo far andare avanti il counter di visibleImg con ++
-// (var visibleImg creata prima del ciclo for e che di fatto "tiene il conto" per noi)
-// inserito if, leggi sotto perché etcc..
-// dopo if creo una var nextImg per l'img a seguire
-// assegno alla img a seguire la classe visible per farla apparire 
-function nextFunction() {
-    const allImg = document.querySelectorAll('.carousel > img');
-    const allTitle = document.querySelectorAll('.carousel > h4');
-    const allParagraph = document.querySelectorAll('.carousel > p');
-    //console.log("all img", allImg);
-    const currentSlideImage = allImg[visibleImg];
-    const currentSlideTitle = allTitle[visibleImg];
-    const currentSlideParagraph = allParagraph[visibleImg];
-    //console.log(currentSlideImage);
-    currentSlideImage.classList.remove("visible");
-    currentSlideTitle.classList.remove("visible");
-    currentSlideParagraph.classList.remove("visible");
-    visibleImg++;
-    if (visibleImg === slidesList.length) {
-        visibleImg = 0;
-    }
-    const nextImg = allImg[visibleImg];
-    const nextTitle = allTitle[visibleImg];
-    const nextParagraph = allParagraph[visibleImg];
-    //console.log(nextImg);
-    nextImg.classList.add("visible")
-    nextTitle.classList.add("visible")
-    nextParagraph.classList.add("visible")
-}
-
-// ora ricopio da sopra ma con -- per fare il previous img
-function prevFunction() {
-    const allImg = document.querySelectorAll('.carousel > img');
-    const allTitle = document.querySelectorAll('.carousel > h4');
-    const allParagraph = document.querySelectorAll('.carousel > p');
-    const currentSlide = allImg[visibleImg];
-    const currentSlideTitle = allTitle[visibleImg];
-    const currentSlideParagraph = allParagraph[visibleImg];
-    //console.log(currentSlide);
-    currentSlide.classList.remove("visible");
-    currentSlideTitle.classList.remove("visible");
-    currentSlideParagraph.classList.remove("visible");
-    visibleImg--;
-    if (visibleImg === -1) {
-        visibleImg = 4;
-    }
-    const prevImg = allImg[visibleImg];
-    const prevTitle = allTitle[visibleImg];
-    const prevParagraph = allParagraph[visibleImg];
-    //console.log(prevImg);
-    prevImg.classList.add("visible")
-    prevTitle.classList.add("visible")
-    prevParagraph.classList.add("visible")
-}
-
-
